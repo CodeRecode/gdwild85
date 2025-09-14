519d861be0aa052ca8621b6bfe22398289833fe6
David Vogel <david@truselo.com>
2025-09-13 17:42:45 -0700

    Hooking up player animations

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,18 +1,14 @@
 extends CharacterBody3D
 class_name Player
 
-@onready var animation_player: AnimationPlayer = $animalchar/AnimationPlayer
-
+@onready var animalchar: Node3D = $animalchar
 
 const SPEED = 5.0
 
 var running: bool = false
-var can_interact: bool = false
+var walking: bool = false
 
-func _ready() -> void:
-	var idle_anim = animation_player.get_animation("Idle")
-	idle_anim.loop_mode = Animation.LOOP_LINEAR
-	animation_player.play("Idle")
+var can_interact: bool = false
 
 func _physics_process(delta: float) -> void:
 	_read_movement_input()
@@ -30,24 +26,26 @@ func _read_movement_input() -> void:
 		input_dir = input_axis
 
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if input_dir.length() >= 0.2:
+		animalchar.look_at(animalchar.global_position - direction)
 
-	if Input.is_action_pressed("run_modifier") or abs(input_h_axis) >= 0.7 or abs(input_v_axis) >= 0.7:
-		running = true
-	else:
-		running = false
+	var should_run = Input.is_action_pressed("run_modifier") or abs(input_h_axis) >= 0.7 or abs(input_v_axis) >= 0.7
 
+	running = false
+	walking = false
 	#run for now
-	if running and input_dir.length() >= 0.2:
+	if should_run and input_dir.length() >= 0.2:
+		running = true
 		velocity.x = direction.x * SPEED * 2
 		velocity.z = direction.z * SPEED * 2
 	#walk for now
 	elif input_dir.length() >= 0.2:
+		walking = true
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 
 func _on_interactable_detector_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
 	print("detected")
